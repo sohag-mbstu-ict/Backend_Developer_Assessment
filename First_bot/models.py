953a5f9a48c06519bcb49830c5faf74d211e5ed4
sohag_00013 (sohag-mbstu-ict) <sohag.hossain.00013@gmail.com>
2023-05-09 18:29:33 +0600

    utomatically search a specific product on Amazon and storing the data in a PostgreSQL database

--- First_bot/models.py
+++ First_bot/models.py
@@ -15,35 +15,38 @@ class ProductDescription(models.Model):
     
     name = models.CharField(max_length=750,blank=True, null=True)
 
-    availability=models.CharField(max_length=15,blank=True, null=True)
+    availability=models.CharField(max_length=50,blank=True, null=True)
 
-    rating=models.CharField(max_length=15 ,default=0,blank=True, null=True)
+    rating=models.FloatField(default=0,blank=True, null=True)
 
-    total_rating=models.CharField(max_length=20 ,default=0,blank=True, null=True)
+    total_rating=models.CharField(max_length=50 ,default=0,blank=True, null=True)
 
-    price=models.CharField(max_length=15 ,default=0,blank=True, null=True)
+    price=models.FloatField(default=0,blank=True, null=True)
 
     shipping_import_details=models.CharField(max_length=100,default="",blank=True, null=True)
 
-    delivery=models.CharField(max_length=30,blank=True, null=True)
+    delivery=models.CharField(max_length=50,blank=True, null=True)
 
-    brand=models.CharField(max_length=20,blank=True, null=True)
+    brand=models.CharField(max_length=50,blank=True, null=True)
 
-    connectivity_technology=models.CharField(max_length=20,blank=True, null=True)
+    connectivity_technology=models.CharField(max_length=50,blank=True, null=True)
 
-    connector_type=models.CharField(max_length=30,blank=True, null=True)
+    connector_type=models.CharField(max_length=50,blank=True, null=True)
 
     compatible_device=models.TextField(max_length=500,blank=True, null=True)
 
     compatible_models=models.TextField(max_length=700,blank=True, null=True)
 
-    # special_features=models.TextField(max_length=300,blank=True, null=True)
+    special_features=models.TextField(max_length=300,blank=True, null=True)
 
-    # color=models.CharField(max_length=15,blank=True, null=True)
+    color=models.CharField(max_length=30,blank=True, null=True)
 
-    # input_voltage=models.CharField(max_length=15,blank=True, null=True)
+    input_voltage=models.CharField(max_length=75,blank=True, null=True)
 
-    # mounting_type=models.CharField(max_length=50,blank=True, null=True)
+    mounting_type=models.CharField(max_length=50,blank=True, null=True)
+
+    #https://stackoverflow.com/questions/1110153/what-is-the-most-efficient-way-to-store-a-list-in-the-django-models
+    img=models.TextField(max_length=3000,null=True)
 
     about_this_item=models.TextField(max_length=2000,blank=True, null=True)
 
--- First_bot/views.py
+++ First_bot/views.py
@@ -28,6 +28,7 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import serializers
+import simplejson as json
 
 from First_bot.models import Product,ProductDescription
 from First_bot.serializers import ProductSerializer,ProductDescriptionSerializer
@@ -107,147 +108,265 @@ def index(request):
 
 
     option=Options()
-    option.headless=True
-    option.add_argument("window-size=1600x900")
+    # option.headless=True
+    # option.add_argument("window-size=1600x900")
 
 
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(options=chrome_options)
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-gpu")
+    option.add_argument("enable-automation")
+    option.add_argument("--headless")
+    option.add_argument("--window-size=1920,1080")
+    option.add_argument("--no-sandbox")
+    option.add_argument("--disable-extensions")
+    option.add_argument("--dns-prefetch-disable")
+    option.add_argument("--disable-gpu")
+    option.page_load_strategy = 'normal'
+    driver = webdriver.Chrome(options=option)
 
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    # web = 'https://www.amazon.com'
-    # driver.get(web)
 
-
-
-    # driver.implicitly_wait(3)
-    # keyword = search
-    # search = driver.find_element(By.ID, 'twotabsearchtextbox')
-    # search.send_keys(keyword)
-    # # click search button
-    # search_button = driver.find_element(By.ID, 'nav-search-submit-button')
-    # search_button.click()
-
-    # driver.implicitly_wait(3)
-
-
-
-    # #driver.maximize_window()
-    # name_l=[]
-    # link_l=[]
-
-
-    # last_page_num=int(driver.find_elements(By.CSS_SELECTOR,"span.s-pagination-item.s-pagination-disabled")[1].text)
-    # print("%%%%%%%%%%%%%%%%%%%%%%%%%%% last_page_num : ",last_page_num)
-
-    # box_l=[]
-
-    # c=0
-    # for _ in range(1,2):
-    #     box=driver.find_elements(By.CSS_SELECTOR,"div.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style")
-    #     c=c+1
-    #     print("ccccccccccccccccccccccccccccccc : ",c)
-    #     for item in box:
-    #         name=item.find_element(By.CSS_SELECTOR,"h2 a span").text
-    #         #print("NNNNNNNNNNNNNNNNNNNNNNNNNNNName : ",name,"   ",c)
-    #         link=item.find_element(By.CSS_SELECTOR,"h2 a")
-    #         #https://stackoverflow.com/questions/36476861/selenium-webelement-object-has-no-attribute-get-attribute
-    #         link=link.get_attribute('href')
-    #         # print("  Linkkkkkkkkkkkkk : ",link)
-            
-    #         link_l.append(link)
-    #         name_l.append(name)
-    #     time.sleep(5)
-    #     new_page=driver.find_element(By.CSS_SELECTOR,"a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator")
-    #     new_page.click()
-    #     time.sleep(3)
     
-    # #print("****************************  name_l : ",link_l,"       ",len(link_l))
-
-    # Product.objects.all().delete()
-    # #Product.objects.create(id=2,name="hello")
-    # for i in range(0,len(link_l)):
-    #     Product.objects.create(id=i,link=link_l[i],name=name_l[i])
-    #     # values=Product(name=name_l[i],link=link_l[i])
-    #     # values.save()
+    web = 'https://www.amazon.com'
+    driver.get(web)
 
 
-    # driver.quit()
-    # # print("name_l : ",name_l,"         ",len(name_l))
-    # #print("ccccccccccccccccccccccccccccccc : ",c)
+
+    driver.implicitly_wait(3)
+    keyword = search
+    search = driver.find_element(By.ID, 'twotabsearchtextbox')
+    search.send_keys(keyword)
+    # click search button
+    search_button = driver.find_element(By.ID, 'nav-search-submit-button')
+    search_button.click()
+
+    driver.implicitly_wait(3)
+
+
+
+    #driver.maximize_window()
+    name_l=[]
+    link_l=[]
+
+
+    last_page_num=int(driver.find_elements(By.CSS_SELECTOR,"span.s-pagination-item.s-pagination-disabled")[1].text)
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%% last_page_num : ",last_page_num)
+
+    box_l=[]
+
+    c=0
+    for _ in range(1,10):
+        box=driver.find_elements(By.CSS_SELECTOR,"div.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style")
+        c=c+1
+        print("ccccccccccccccccccccccccccccccc : ",c)
+        for item in box:
+            name=item.find_element(By.CSS_SELECTOR,"h2 a span").text
+            #print("NNNNNNNNNNNNNNNNNNNNNNNNNNNName : ",name,"   ",c)
+            link=item.find_element(By.CSS_SELECTOR,"h2 a")
+            #https://stackoverflow.com/questions/36476861/selenium-webelement-object-has-no-attribute-get-attribute
+            link=link.get_attribute('href')
+            # print("  Linkkkkkkkkkkkkk : ",link)
+            
+            link_l.append(link)
+            name_l.append(name)
+        time.sleep(5)
+        new_page=driver.find_element(By.CSS_SELECTOR,"a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator")
+        new_page.click()
+        time.sleep(3)
+    
+    #print("****************************  name_l : ",link_l,"       ",len(link_l))
+
+    #Product.objects.all().delete()
+    #Product.objects.create(id=2,name="hello")
+    for i in range(0,len(link_l)):
+        #Product.objects.create(id=i,link=link_l[i],name=name_l[i])
+        values=Product(id=i,link=link_l[i],name=name_l[i])
+        values.save()
+
+    count= Product.objects.all().count()
+    print("count /////////////////////// ",count)
+    
+    # print("name_l : ",name_l,"         ",len(name_l))
+    #print("ccccccccccccccccccccccccccccccc : ",c)
 
 ##################################################################################################
     # #Product.objects.all().delete()
     # #Product.objects.create(id=2,name="hello")
-    obj=Product.objects.all().values()
+    #obj=Product.objects.all().values()
     #print("$$$$$$$$$$$$$$$$$ obj obj : ",obj)
-    id=Product.objects.values_list('id',flat=True)
+    #id=Product.objects.values_list('id',flat=True)
     #print("$$$$$$$$$$$$$$$$$ idddddddddd : ",id)
 
     # #https://betterprogramming.pub/understanding-django-database-querysets-and-its-optimizations-1765cb9c36e5
     c=0
-    for each in Product.objects.all():
-        #print("each.link   :   ",each.link)
-        a=each.link
-        c=c+1
-        break
-        # if(c==2):
-        #     break
-    print("aaaaaaaaaaaaaaaaaaaaaaaa web   :   ",a)
-    web=a
-    #web = 'https://www.amazon.com/gp/slredirect/picassoRedirect.html/ref=pa_sp_atf_aps_sr_pg1_1?ie=UTF8&adId=A0300086DVV103RTJFEJ&qualifier=1683472286&id=5670395769173738&widgetName=sp_atf&url=%2FiOttie-Compatible-Midnight-Charging-Including%2Fdp%2FB0C1DHHG9N%2Fref%3Dsr_1_2_sspa%3Fkeywords%3Dwireless%2Bcharger%26qid%3D1683472286%26sr%3D8-2-spons%26psc%3D1'
-    time.sleep(3)
-    driver.implicitly_wait(7)
-    driver.get(web)
-    time.sleep(15)
-    driver.implicitly_wait(15)
-    name= WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR,"span#productTitle.a-size-medium.product-title-word-break.product-title-resize"))).text
-    #name=driver.find_element(By.CSS_SELECTOR,"span#productTitle.a-size-medium.product-title-word-break.product-title-resize").text
-    time.sleep(2)
-    availability=driver.find_element(By.CSS_SELECTOR,"span.a-size-medium.a-color-success").text
-    time.sleep(2)
-    rating=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="acrPopover"]/span[1]/a/span'))).text
-    #time.sleep(2)
-    total_rating=driver.find_element(By.XPATH,'//*[@id="acrCustomerReviewText"]').text
-    #time.sleep(2)
-    price=driver.find_element(By.CSS_SELECTOR,"span.a-price-whole").text
-    #time.sleep(2)
-    shipping_import_details=driver.find_element(By.XPATH,'//*[@id="amazonGlobal_feature_div"]/span[1]').text
-    #time.sleep(2)
-    delivery=driver.find_element(By.XPATH,'//*[@id="mir-layout-DELIVERY_BLOCK-slot-PRIMARY_DELIVERY_MESSAGE_LARGE"]/span/span[1]').text
-    #time.sleep(2)
-    brand=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[1]/td[2]/span').text
-    #time.sleep(2)
-    connectivity_technology=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[2]/td[2]/span').text
-
-    connector_type=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[3]/td[2]/span').text
-
-    compatible_device=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[4]/td[2]/span').text
-
-    compatible_models=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[5]/td[2]/span[1]/span/span[2]').text
-    time.sleep(2)
-    #special_features=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[7]/td[2]/span[1]/span/span[2]').text
-    #time.sleep(2)
-    # color=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[8]/td[2]/span').text
-    # time.sleep(2)
-    # input_voltage=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[9]/td[2]/span').text
-    # time.sleep(2)
-    # mounting_type=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[10]/td[2]/span').text
-    # time.sleep(2)
-    about_this_item=driver.find_element(By.XPATH,'//*[@id="feature-bullets"]/ul').text
-
-
     ProductDescription.objects.all().delete()
-    values_=ProductDescription(genre_id=1,name=name,availability=availability,rating=rating,
-                total_rating=total_rating,price=price,shipping_import_details=shipping_import_details,
-                delivery=delivery,brand=brand,connectivity_technology=connectivity_technology,
-                connector_type=connector_type,compatible_device=compatible_device,
-                compatible_models=compatible_models,about_this_item=about_this_item)
-    values_.save()
+    for each in Product.objects.all():
+        c=c+1
+        if(c>=count):
+            break
+        #print("each.link   :   ",each.link)
+        idd=each.id
+        a=each.link
+        #break  
+        print("cccccccccccccccccccccccccc   :   ",c)
+        print("web web  web web web web web web   :   ",a)
+        web=a
+        #web = 'https://www.amazon.com/Apple-MHXH3AM-A-MagSafe-Charger/dp/B08L5NP6NG/ref=sr_1_4?keywords=wireless+charger&qid=1683472286&sr=8-4'
+        driver.get(web)
+        time.sleep(5)
+        
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="productTitle"]')):
+                name=driver.find_element(By.XPATH,'//*[@id="productTitle"]').text
+        except:
+            name="Currently unavailable"
 
-    print("1111111111nameeeeeeeee name : ",rating)
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="availability"]/span')):
+                availability=driver.find_element(By.XPATH,'//*[@id="availability"]/span').text
+        except:
+            availability="Currently unavailable"
+        
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="acrPopover"]/span[1]/a/span')):
+                rating=float(driver.find_element(By.XPATH,'//*[@id="acrPopover"]/span[1]/a/span').text)
+                print("   rating gggggg  : ",rating,"    type   : ",type(rating))
+        except:
+            rating=0
+        
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="acrCustomerReviewText"]')):
+                total_rating=driver.find_element(By.XPATH,'//*[@id="acrCustomerReviewText"]').text
+        except:
+            total_rating="Currently unavailable"
+
+        #First XPATH price
+        ck_price=0
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[2]/span[2]/span[2]')):
+                price=float(driver.find_element(By.XPATH,'//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[2]/span[2]/span[2]').text)
+                ck_price=1
+        except:
+          price=0
+
+        #Second XPATH for price
+        if(ck_price==0):
+            try:
+                if(driver.find_element(By.XPATH,'//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[1]/span[2]/span[2]')):
+                        price=float(driver.find_element(By.XPATH,'//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[1]/span[2]/span[2]').text)
+            except:
+                price=0
+
+        
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="amazonGlobal_feature_div"]/span[1]')):
+                shipping_import_details=driver.find_element(By.XPATH,'//*[@id="amazonGlobal_feature_div"]/span[1]').text
+        except:
+            shipping_import_details="Currently unavailable" 
+            
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="mir-layout-DELIVERY_BLOCK-slot-PRIMARY_DELIVERY_MESSAGE_LARGE"]/span/span[1]')):
+                delivery=driver.find_element(By.XPATH,'//*[@id="mir-layout-DELIVERY_BLOCK-slot-PRIMARY_DELIVERY_MESSAGE_LARGE"]/span/span[1]').text
+        except:
+            delivery="Currently unavailable" 
+        
+        #First XPATH of brand
+        ck_brand=0
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[1]/td[2]/span')):
+                brand=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[1]/td[2]/span').text
+                ck_brand=1
+        except:
+            brand="Currently unavailable"
+
+        #Second XPATH for brand
+        if(ck_brand==0):
+            try:
+                if(driver.find_element(By.XPATH,'//*[@id="productOverview_feature_div"]/div/table/tbody/tr[1]/td[2]/span')):
+                        brand=driver.find_element(By.XPATH,'//*[@id="productOverview_feature_div"]/div/table/tbody/tr[1]/td[2]/span').text
+            except:
+                brand="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[2]/td[2]/span')):
+                connectivity_technology=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[2]/td[2]/span').text
+        except:
+            connectivity_technology="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[3]/td[2]/span')):
+                connector_type=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[3]/td[2]/span').text
+        except:
+            connector_type="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[4]/td[2]/span[1]/span/span[2]')):
+                compatible_device=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[4]/td[2]/span[1]/span/span[2]').text
+        except:
+            compatible_device="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[5]/td[2]/span[1]/span/span[2]')):
+                compatible_models=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[5]/td[2]/span[1]/span/span[2]').text
+        except:
+            compatible_models="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[7]/td[2]/span')):
+                special_features=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[7]/td[2]/span').text
+        except:
+            special_features="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[8]/td[2]/span')):
+                color=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[8]/td[2]/span').text
+        except:
+            color="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[9]/td[2]/span')):
+                input_voltage=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[9]/td[2]/span').text
+        except:
+            input_voltage="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[10]/td[2]/span')):
+                mounting_type=driver.find_element(By.XPATH,'//*[@id="poExpander"]/div[1]/div/table/tbody/tr[10]/td[2]/span').text
+        except:
+            mounting_type="Currently unavailable"
+
+        try:
+            if(driver.find_element(By.XPATH,'//*[@id="feature-bullets"]/ul')):
+                about_this_item=driver.find_element(By.XPATH,'//*[@id="feature-bullets"]/ul').text
+        except:
+            about_this_item="Currently unavailable"
+
+        #https://stackoverflow.com/questions/1110153/what-is-the-most-efficient-way-to-store-a-list-in-the-django-models
+        #https://stackoverflow.com/questions/65531814/extract-all-the-src-attribute-from-the-images-of-amazon-product-page-in-selenium
+        img_l=[]
+        my_img=ProductDescription()
+        #for my_elem in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@id='altImages']/ul//li[@data-ux-click]//img"))):
+        time.sleep(2)
+        for my_elem in driver.find_elements(By.CSS_SELECTOR,'#a-autoid-4-announce >img'):
+            img_l.append(my_elem.get_attribute("src"))
+        #my_img.img=json.dumps(img_l)
+        #driver.find_elements(By.CSS_SELECTOR,
+
+        #ProductDescription.objects.all().delete()
+        print("1111111111nameeeeeeeee name : ",rating)
+        values_=ProductDescription (genre_id=c,id=c,name=name,availability=availability,rating=rating,
+                    total_rating=total_rating,price=price,shipping_import_details=shipping_import_details,
+                    delivery=delivery,brand=brand,about_this_item=about_this_item,img=json.dumps(img_l),
+                    connectivity_technology=connectivity_technology,connector_type=connector_type,
+                    compatible_device=compatible_device,compatible_models=compatible_models,color=color,
+                    special_features=special_features,input_voltage=input_voltage,
+                    mounting_type=mounting_type)
+        values_.save()
+
+        #print("1111111111nameeeeeeeee name : ",rating)
+        #break
+    driver.quit()
 
     return HttpResponse("Hello, world. You're at the polls index.")
